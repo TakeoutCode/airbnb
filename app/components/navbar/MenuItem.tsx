37251a6286a09c4d601bf81f10d7de025159dd1e
Braulio Quezada <takeoutcode@gmail.com>
2023-05-22 15:17:59 -0500

    feat: Add ui navbar

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { FC } from 'react';
+
+interface MenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+
+export const MenuItem: FC<MenuItemProps> = ({ onClick, label }) => {
+  return (
+    <div
+      onClick={onClick}
+      className='
+    px-4
+    py-3
+    hover:py-neutral-100
+    transition
+    font-semibold
+
+  '
+    >
+      {label}
+    </div>
+  );
+};
