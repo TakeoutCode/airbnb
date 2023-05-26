7884865dfa007381e3d5328926599d8935dc2db8
Braulio Quezada <takeoutcode@gmail.com>
2023-05-26 15:03:17 -0500

    feat: Add basic info rentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Heading } from '@components/Heading';
 import { CategoryInput } from '@components/inputs/CategoryInput';
+import { Counter } from '@components/inputs/Counter';
 import { CountrySelect } from '@components/inputs/CountrySelect';
 import { Modal } from '@components/modals/Modal';
 import { categories } from '@components/navbar/Categories';
@@ -46,6 +47,9 @@ export const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
+  const guestCount = watch('guestCount');
+  const roomCount = watch('roomCount');
+  const bathroomCount = watch('bathroomCount');
 
   const Map = useMemo(
     () =>
@@ -128,6 +132,37 @@ export const RentModal = () => {
           onChange={(value) => setCustomValue('location', value)}
         />
         <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title={'Share some basics about your place'}
+          subtitle='What amenities do you have?'
+        />
+        <Counter
+          title={'Guests'}
+          subtitle={'How many guests do you allow?'}
+          value={guestCount}
+          onChange={(value) => setCustomValue('guestCount', value)}
+        />
+
+        <Counter
+          title={'Rooms'}
+          subtitle={'How many rooms do you have?'}
+          value={roomCount}
+          onChange={(value) => setCustomValue('roomCount', value)}
+        />
+        <hr />
+        <Counter
+          title={'Bathrooms'}
+          subtitle={'How many Bathrooms do you have?'}
+          value={bathroomCount}
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
       </div>
     );
   }
